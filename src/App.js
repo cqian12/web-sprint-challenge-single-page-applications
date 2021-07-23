3f93fb9d82b6da48bb5667429aa5ba388648e666
Charles Qian <charleshqian@gmail.com>
2021-07-23 10:30:52 -0700

    answered interview questions, installed necessary modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { axios } from '../node_modules/axios'
+import { Route, Switch } from "react-router";
+
+import Pizza from './Pizza'
 
 const App = () => {
+const [order,setOrder] = useState([])
+
   return (
-    <>
+    <div>
       <h1>Lambda Eats</h1>
-    </>
+
+      <Switch>
+        <Route path = '/pizza-order'>
+          <Pizza />
+        </Route>
+      </Switch>
+    </div>
   );
 };
+
 export default App;
