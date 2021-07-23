54283d4f4b47f6ce74c82d3340f437a4ba48f5cc
Charles Qian <charleshqian@gmail.com>
2021-07-23 16:20:06 -0700

    added comments

--- src/Order.js
+++ src/Order.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+//this isn't currently in use but would be used to show a summary of each order
+//I thought we were outputting the orders to the page but they are in console log instead
+
 export default function Order({info}) {
     if (!info) {
         return <h2>Getting order info...</h2>
@@ -7,7 +10,7 @@ export default function Order({info}) {
 
     return (
         <div>
-          <h2>Order from: {info.name}</h2>
+          <h3>Order from: {info.name}</h3>
           <p>Size: {info.size}</p>
           <ul>Toppings</ul>
           <li>Nuts: {info.nuts ? 'Yes' : 'No'}</li>
